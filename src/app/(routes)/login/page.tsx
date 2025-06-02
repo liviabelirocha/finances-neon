e61375ee577b26f39fc1991400c636ddb932d2cf
Lívia Belizário <e-livia.rocha@allos.co>
2025-06-02 09:48:38 -0300

    feat: make login page minimalist

--- src/app/(routes)/login/page.tsx
+++ src/app/(routes)/login/page.tsx
@@ -3,7 +3,6 @@ import { Button } from "@/_components/ui/button";
 import { SignInButton } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
 import { LogInIcon } from "lucide-react";
-import Image from "next/image";
 import { redirect } from "next/navigation";
 
 export default async function LoginPage({
@@ -16,26 +15,19 @@ export default async function LoginPage({
   if (userId) redirect(searchParams.redirectTo || "/");
 
   return (
-    <div className="grid h-full grid-cols-2">
-      <div className="mx-auto flex h-full max-w-[550px] flex-col justify-center p-8">
-        <Logo />
+    <div className="mx-auto flex h-full flex-col justify-center p-8">
+      <Logo />
 
-        <h1 className="mb-3 text-4xl font-bold">Welcome</h1>
-        <p className="mb-8 text-muted-foreground">
-          Finances Neon is a financial management platform that uses AI to
-          monitor your transactions and provide personalized insights, making it
-          easier to manage your budget.
-        </p>
-        <SignInButton>
-          <Button variant="outline">
-            <LogInIcon className="mr-2" />
-            Sign in or create account
-          </Button>
-        </SignInButton>
-      </div>
-      <div className="relative h-full w-full">
-        <Image src="/login.png" alt="Login" fill className="object-cover" />
-      </div>
+      <h1 className="mb-3 text-4xl font-bold">Welcome</h1>
+      <p className="mb-8 text-muted-foreground">
+        Finances Neon is a financial management platform.
+      </p>
+      <SignInButton>
+        <Button variant="outline">
+          <LogInIcon className="mr-2" />
+          Sign in or create account
+        </Button>
+      </SignInButton>
     </div>
   );
 }
