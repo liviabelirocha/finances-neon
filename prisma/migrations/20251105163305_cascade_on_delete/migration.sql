c7de7f8c4adb27dc70b295123b4ab634666b8f1e
Lívia Belizário <liviabelirocha@outlook.com>
2025-11-05 13:34:19 -0300

    fix: delete cascade

--- prisma/migrations/20251105163305_cascade_on_delete/migration.sql
+++ prisma/migrations/20251105163305_cascade_on_delete/migration.sql
@@ -1,11 +1,11 @@
 -- DropForeignKey
-ALTER TABLE "Tag" DROP CONSTRAINT "Tag_boardId_fkey";
+ALTER TABLE "public"."Tag" DROP CONSTRAINT "Tag_boardId_fkey";
 
 -- DropForeignKey
-ALTER TABLE "Transaction" DROP CONSTRAINT "Transaction_boardId_fkey";
+ALTER TABLE "public"."Transaction" DROP CONSTRAINT "Transaction_boardId_fkey";
 
 -- DropForeignKey
-ALTER TABLE "UserBoard" DROP CONSTRAINT "UserBoard_boardId_fkey";
+ALTER TABLE "public"."UserBoard" DROP CONSTRAINT "UserBoard_boardId_fkey";
 
 -- AddForeignKey
 ALTER TABLE "UserBoard" ADD CONSTRAINT "UserBoard_boardId_fkey" FOREIGN KEY ("boardId") REFERENCES "Board"("id") ON DELETE CASCADE ON UPDATE CASCADE;
